077b2058385fcaac3e0615a63c04ee52491772bf
fmkar <fmkar@MSI>
2025-09-07 17:03:53 +0200

    Add project files.

---
+++ Kutuphane/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Kutuphane.Models
+{
+    public enum ReadingStatus { ToRead, Reading, Read, Favorite }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Author { get; set; } = default!;
+        public string Genre { get; set; } = default!;
+        public ReadingStatus Status { get; set; } = ReadingStatus.ToRead;
+        public string? Notes { get; set; }
+    }
+}
